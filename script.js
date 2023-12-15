055dd096afca60315b164cf48bdae7c0cf23c210
erudit aoue <philaoue@gmail.com>
2023-12-15 14:35:19 +0100

    mise en place de mon javascript

---
+++ script.js
@@ -0,0 +1,31 @@
+let sp, h, m, s, ms, btnStart, btnReset, btnStop, 
+
+//commençons par créer une boucle
+
+() => {
+    sp = document.querySelectorAll("span");
+    btnStart =document.getElementById("start");
+    btnStop = document.getElementById("stop");
+    btnReset = document.getElementById("reset");
+
+    
+h=0;
+m=0;
+s=0;
+ms=0;
+
+    btnStart.addEventListener("click", ()=>(
+        console.log("je fonctionne correctement")
+         
+    ));
+    btnStop.addEventListener("click", ()=> (
+
+        console.log("je fonctionne correctement")
+    ));
+
+    btnReset..addEventListener("click", ()=> (
+
+        console.log("je fonctionne correctement")
+        ));
+
+}
